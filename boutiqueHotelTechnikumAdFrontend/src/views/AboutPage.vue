a3250a7037f8ebffe7d077cae9a3bf498dad703d
Sonatai <gebauerjenny@yahoo.de>
2024-05-29 18:37:20 +0200

    add container component

--- boutiqueHotelTechnikumAdFrontend/src/views/AboutPage.vue
+++ boutiqueHotelTechnikumAdFrontend/src/views/AboutPage.vue
@@ -16,13 +16,13 @@
         </ion-toolbar>
       </ion-header>
       <h1>About der Boutique Hotel Technikum</h1>
-      <div id="container">
+      <content-container>
         <!--
           Cool description of what we do
 
           Nice Head-Shots
         -->
-      </div>
+      </content-container>
     </ion-content>
   </ion-page>
 </template>
@@ -37,31 +37,5 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/vue";
+import ContentContainer from "../components/ContentContainer.vue";
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 1.25rem;
-  line-height: 1.625rem;
-}
-
-#container p {
-  font-size: 1rem;
-  line-height: 1.375rem;
-  color: #8c8c8c;
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
--- boutiqueHotelTechnikumAdFrontend/src/views/ImpressumPage.vue
+++ boutiqueHotelTechnikumAdFrontend/src/views/ImpressumPage.vue
@@ -17,7 +17,7 @@
       </ion-header>
       <h1>Impressum der Boutique Hotel Technikum</h1>
       <!-- TODO: eigene componente für den container -->
-      <div id="container">
+      <content-container>
         <p>Für den Inhalt auf dieser Website verantwortlich</p>
         <h2>Hotel Schani GmbH</h2>
         <p>
@@ -25,7 +25,7 @@
           Example-Straße 42<br />
           1100 Wien, Austria<br />
         </p>
-      </div>
+      </content-container>
     </ion-content>
   </ion-page>
 </template>
@@ -40,31 +40,5 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/vue";
+import ContentContainer from "../components/ContentContainer.vue";
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 1.25rem;
-  line-height: 1.625rem;
-}
-
-#container p {
-  font-size: 1rem;
-  line-height: 1.375rem;
-  color: #8c8c8c;
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
