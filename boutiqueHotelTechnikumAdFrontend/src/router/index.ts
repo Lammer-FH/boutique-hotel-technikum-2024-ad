a6869b9d95f2845b434b156b1ab34f39dbc8ff05
Sonatai <gebauerjenny@yahoo.de>
2024-06-05 18:59:01 +0200

    move route urls to config file

--- boutiqueHotelTechnikumAdFrontend/src/router/index.ts
+++ boutiqueHotelTechnikumAdFrontend/src/router/index.ts
@@ -1,32 +1,33 @@
-import {createRouter, createWebHistory} from "@ionic/vue-router";
-import {RouteRecordRaw} from "vue-router";
+import { routeUrls } from "@/navigation.config";
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: "",
-        component: () => import("../views/LandingPage.vue"),
-    },
-    {
-        path: "/about",
-        component: () => import("../views/AboutPage.vue"),
-    },
-    {
-        path: "/impressum",
-        component: () => import("../views/ImpressumPage.vue"),
-    },
-    {
-        path: '/rooms',
-        component: () => import ('../views/RoomsPage.vue')
-    },
-    {
-        path: '/rooms/:id',
-        component: () => import ('../views/RoomDetailPage.vue')
-    },
+  {
+    path: routeUrls.home,
+    component: () => import("../views/LandingPage.vue"),
+  },
+  {
+    path: routeUrls.about,
+    component: () => import("../views/AboutPage.vue"),
+  },
+  {
+    path: routeUrls.impressum,
+    component: () => import("../views/ImpressumPage.vue"),
+  },
+  {
+    path: routeUrls.rooms,
+    component: () => import("../views/RoomsPage.vue"),
+  },
+  {
+    path: routeUrls.roomDetails,
+    component: () => import("../views/RoomDetailPage.vue"),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
