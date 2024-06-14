32bfc7d77f654bac0b9c4afa9fccc752be2bf570
Sonatai <gebauerjenny@yahoo.de>
2024-06-14 18:35:59 +0200

    add key to list

--- boutiqueHotelTechnikumAdFrontend/src/components/ExtrasDisplay.vue
+++ boutiqueHotelTechnikumAdFrontend/src/components/ExtrasDisplay.vue
@@ -1,34 +1,39 @@
 <template>
-  <ion-item>
-    <ion-badge class="ion-margin" v-for="extra in extras">
-      <ion-icon size="large" :name="getIconName(extra.name)">{{ extra }}</ion-icon>
-    </ion-badge>
-  </ion-item>
-
+    <div>
+        <ion-badge class="ion-margin" v-for="extra in extras" :key="extra">
+            <ion-icon size="large" :name="getIconName(extra.name)">{{
+                extra
+            }}</ion-icon>
+        </ion-badge>
+    </div>
 </template>
 
 <script lang="ts">
-import {IonIcon} from '@ionic/vue';
+import { IonIcon } from '@ionic/vue';
 
 export default {
-  components: {IonIcon },
-  props: {
-    extras: {
-      type: Array,
-      required: true
+    components: { IonIcon },
+    props: {
+        extras: {
+            type: Array,
+            required: true,
+        },
     },
-  },
-  methods: {
-    getIconName(name : string) {
-      switch (name) {
-        case 'AIR_CONDITIONING': return 'snow-outline';
-        case 'FREE_WIFI': return 'wifi-outline';
-        case 'BALCONY' : return 'image-outline';
-        case 'FLAT_SCREEN_TV': return 'tv-outline';
-        default: return 'add-circle-outline';
-      }
-    }
-  }
-}
+    methods: {
+        getIconName(name: string) {
+            switch (name) {
+                case 'AIR_CONDITIONING':
+                    return 'snow-outline';
+                case 'FREE_WIFI':
+                    return 'wifi-outline';
+                case 'BALCONY':
+                    return 'image-outline';
+                case 'FLAT_SCREEN_TV':
+                    return 'tv-outline';
+                default:
+                    return 'add-circle-outline';
+            }
+        },
+    },
+};
 </script>
-<style scoped></style>
