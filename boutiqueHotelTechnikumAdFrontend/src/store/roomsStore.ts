959ad204653963c603e7727e6d4a997e524b9845
Sonatai <gebauerjenny@yahoo.de>
2024-06-14 19:42:39 +0200

    fix room state

--- boutiqueHotelTechnikumAdFrontend/src/store/roomsStore.ts
+++ boutiqueHotelTechnikumAdFrontend/src/store/roomsStore.ts
@@ -1,30 +1,39 @@
-import { defineStore } from "pinia";
-import axios from "axios";
-import { Room } from "@/model/room";
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import { Room } from '@/model/room';
 
-const apiUrl = import.meta.env.VITE_APP_API_URL;
+const apiUrl = 'http://localhost:8081/api';
 
 export const useRoomStore = defineStore('room', {
     state: () => {
         return {
             rooms: [] as Room[],
             selectedRoom: Room.prototype,
-            filter: {to: new Date(), from: new Date()},
-        }
+            filter: { to: new Date(), from: new Date() },
+        };
     },
     actions: {
-        async getRooms(from: Date = new Date(1000, 1, 1),
-                       to: Date = new Date(3000, 12, 31)) {
+        async getRooms(
+            from: Date = new Date(1000, 1, 1),
+            to: Date = new Date(3000, 12, 31)
+        ) {
             if (apiUrl !== undefined) {
-                return axios.get<Room[]>(apiUrl + "/rooms?from=" +
-                    from.toISOString().split('T')[0] +
-                    "&to=" + to.toISOString().split('T')[0], {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                }).then(response => {
-                    this.rooms = response.data;
-                });
+                return axios
+                    .get<Room[]>(
+                        apiUrl +
+                            '/rooms?from=' +
+                            from.toISOString().split('T')[0] +
+                            '&to=' +
+                            to.toISOString().split('T')[0],
+                        {
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                        }
+                    )
+                    .then((response) => {
+                        this.rooms = response.data;
+                    });
             }
         },
         selectRoom(room: Room) {
@@ -33,20 +42,6 @@ export const useRoomStore = defineStore('room', {
         setFilter(from: string, to: string) {
             this.filter.to = new Date(to);
             this.filter.from = new Date(from);
-        }
-        return axios
-          .get<Room[]>(apiUrl + "/rooms?from=" + from + "&to=" + to, {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-          .then((response) => {
-            this.rooms = response.data;
-          });
-      }
+        },
     },
-    selectRoom(room: Room) {
-      this.selectedRoom = room;
-    },
-  },
 });
